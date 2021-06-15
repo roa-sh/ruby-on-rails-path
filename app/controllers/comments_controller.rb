a2381d52b3bd3d0743715030e47a4ef68b4cf661
roa-sh <juanmanuelrm2009@gmail.com>
2021-06-15 12:00:13 -0500

    Bootstrap implementtion
    Bug on routes when destroy
    
    dropdown fixed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
-  def create 
+
+
+  def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
@@ -9,6 +11,7 @@ class CommentsController < ApplicationController
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
+
     redirect_to article_path(@article)
   end
 
